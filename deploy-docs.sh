10cb7b0ae069e10276d5ab20c769a10d4557f076
Matt Traudt <sirmatt@ksu.edu>
2020-12-03 12:32:07 -0500

    Don't itemize changes with rsync

--- deploy-docs.sh
+++ deploy-docs.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -eu
 cargo doc
-rsync -air --delete target/doc/ piggie:/var/www/demos.traudt.xyz/aoc2020
+rsync -ar --delete target/doc/ piggie:/var/www/demos.traudt.xyz/aoc2020
